26a0303690fce14c00f095d38645c7946c7d16fa
kelvi <kelvi_ugo@hotmail.com>
2022-03-09 23:16:18 -0300

    feat: instala storybook e configura

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -7,3 +7,14 @@ export const parameters = {
     },
   },
 }
+
+import {theme} from '../src/styles/theme';
+import { ThemeProvider } from 'styled-components';
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <Story />
+    </ThemeProvider>
+  ),
+];
