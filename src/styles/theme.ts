8f3550cf35e63fcadb36325a9ebcb14df1ee979c
kelvi <kelvi_ugo@hotmail.com>
2022-03-10 19:23:58 -0300

    refactor: altera theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,49 +1,51 @@
 export const theme = {
-    grid: {
-      container: "130rem",
+  grid: {
+    container: "130rem",
+  },
+  colors: {
+    primary: "#ea1d2c",
+    secondary: "#b6d048",
+    background: "#f7f7f7",
+    backgroundLight: "#ffffff",
+
+    black: "#000000",
+  },
+  font: {
+    family:
+      "Poppins, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+    light: 300,
+    normal: 400,
+    bold: 500,
+    sizes: {
+      xsmall: "1.2rem",
+      small: "1.4rem",
+      medium: "1.6rem",
+      large: "1.8rem",
+      xlarge: "2.0rem",
+      xxlarge: "2.8rem",
+      huge: "5.2rem",
     },
     colors: {
-      primary: "#ea1d2c",
-      secondary: "#b6d048",
-      background: "#f7f7f7",
-      backgroundLight: "#ffffff",
-  
-      black: "#000000",
+      black: "#3e3e3e",
+      darkGray: "#717171",
+      gray: "#a6a6a6",
+      white: "#ffffff",
     },
-    font: {
-      family:
-        "Poppins, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
-      light: 300,
-      normal: 400,
-      bold: 500,
-      sizes: {
-        xsmall: "1.2rem",
-        small: "1.4rem",
-        medium: "1.6rem",
-        large: "1.8rem",
-        xlarge: "2.0rem",
-        xxlarge: "2.8rem",
-        huge: "5.2rem",
-      },
-      colors: {
-        primary: "#3e3e3e",
-        secondary: "#ffffff",
-      },
-    },
-    border: {
-      radius: "0.6rem",
-    },
-    spacings: {
-      xxsmall: "0.8rem",
-      xsmall: "1.6rem",
-      small: "2.4rem",
-      medium: "3.2rem",
-      large: "4.0rem",
-      xlarge: "4.8rem",
-      xxlarge: "5.6rem",
-    },
-    transition: {
-      default: "0.3s ease-in-out",
-      fast: "0.1s ease-in-out",
-    },
+  },
+  border: {
+    radius: "0.6rem",
+  },
+  spacings: {
+    xxsmall: "0.8rem",
+    xsmall: "1.6rem",
+    small: "2.4rem",
+    medium: "3.2rem",
+    large: "4.0rem",
+    xlarge: "4.8rem",
+    xxlarge: "5.6rem",
+  },
+  transition: {
+    default: "0.3s ease-in-out",
+    fast: "0.1s ease-in-out",
+  },
 };
