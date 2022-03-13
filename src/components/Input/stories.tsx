1580c6db3cfbc1649a7436777d933ff43bf69b78
kelvi <kelvi_ugo@hotmail.com>
2022-03-13 12:25:13 -0300

    feat: adiciona testes para input e finaliza storybook

--- src/components/Input/stories.tsx
+++ src/components/Input/stories.tsx
@@ -4,15 +4,31 @@ import { FiMapPin } from 'react-icons/fi'
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { Input } from '.';
 
-
 export default {
     title: 'Input',
     component: Input,
+    argTypes: {
+        icon: {
+            control: {
+                type: null
+            }
+        }
+    }
 } as ComponentMeta<typeof Input>;
 
 export const Default: ComponentStory<typeof Input> = (args) => (
     <Input {...args} />
 );
 Default.args = {
-    // icon: <FiMapPin />
+    placeholder: 'teste placeholder'
 };
+
+export const WithIcon: ComponentStory<typeof Input> = (args) => (
+    <Input {...args} />
+);
+WithIcon.args = {
+    placeholder: 'teste placeholder',
+    icon: <FiMapPin />
+};
+
+
---
+++ src/components/Input/test.spec.tsx
@@ -0,0 +1,48 @@
+import { screen, waitFor } from "@testing-library/react"
+import { render } from "utils/testUtils"
+import userEvent from '@testing-library/user-event'
+import { Input } from "."
+import { AiFillAlert } from 'react-icons/ai'
+
+describe("<Input />", () => {
+    it("shold should be render an Input", () => {
+        render(<Input />)
+
+        expect(screen.getByRole('textbox')).toBeDefined()
+    })
+
+    it("shold should be render an Input with placeholder", () => {
+        render(<Input placeholder="teste placeholder" />)
+
+        expect(screen.getByPlaceholderText('teste placeholder')).toBeInTheDocument()
+    })
+
+    it("shold should be render an Input with icon", () => {
+        render(<Input icon={<AiFillAlert data-testid="icon" />} />)
+
+        expect(screen.getByTestId('icon')).toBeInTheDocument()
+    })
+
+    it("Changes its value when typing", async () => {
+        const onInputChange = jest.fn()
+        render(
+            <Input
+                placeholder="teste de digitação"
+                onChange={onInputChange}
+            />
+        )
+
+        const input = screen.getByPlaceholderText('teste de digitação')
+        const text = "O rato roeu a ropa do rei de Roma "
+
+        userEvent.type(input, text)
+
+        await waitFor(() => {
+            expect(input).toHaveValue(text)
+            expect(onInputChange).toHaveBeenCalledTimes(text.length)
+        })
+
+    })
+
+
+})
