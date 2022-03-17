17efc1754f0259d6159d9ec42bcdc66ae7fa9b4f
kelvi <kelvi_ugo@hotmail.com>
2022-03-16 22:34:03 -0300

    feat: finaliza teste no componente Button

--- src/components/Button/test.spec.tsx
+++ src/components/Button/test.spec.tsx
@@ -1,19 +1,42 @@
-import { render, screen } from "@testing-library/react";
-import { ThemeProvider } from "styled-components";
+import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { theme } from "styles/theme";
+import { render } from "utils/testUtils";
 import { Button } from ".";
 
 describe("<Button />", () => {
-  it("shold render a Button", () => {
+  it("shold render a Button default", () => {
     render(
-      <ThemeProvider theme={theme}>
-        <Button>Teste Button</Button>
-      </ThemeProvider>
+      <Button>Teste Button</Button>
     );
 
     expect(screen.getByRole("button", { name: /Teste Button/i })).toHaveStyle({
       backgroundColor: theme.colors.primary,
       color: theme.font.colors.white,
     });
+  });
+
+  it("shold render a Button transparent", () => {
+    render(
+      <Button variant="transparent">Teste Button</Button>
+    );
+
+    expect(screen.getByRole("button", { name: /Teste Button/i })).toHaveStyle({
+      backgroundColor: "transparent",
+      color: theme.colors.primary,
+    });
+  });
+
+
+  it("testing onClick", () => {
+    const functionTest = jest.fn()
+    render(
+      <Button onClick={functionTest}>Teste Button</Button>
+    );
+    const button = screen.getByRole("button", { name: /Teste Button/i })
+    userEvent.click(button)
+
+    expect(functionTest).toBeCalled()
+
   });
 });
