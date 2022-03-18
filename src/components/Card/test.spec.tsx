ec78057635f406596788659462aa5b802dc33683
kelvi <kelvi_ugo@hotmail.com>
2022-03-17 21:40:05 -0300

    test: adiciona testes para Card

--- src/components/Card/test.spec.tsx
+++ src/components/Card/test.spec.tsx
@@ -3,16 +3,37 @@ import { theme } from "styles/theme"
 import { render } from "utils/testUtils"
 import { Card } from "."
 
+const props = {
+    image: "https://static-images.ifood.com.br/image/upload/f_auto/webapp/landingV2/restaurant.png",
+    title: "Restaurante",
+    textButton: "Ver opções"
+}
+
 describe('<Card />', () => {
     it('should be render a card default', () => {
-        render(<Card
-            image="https://static-images.ifood.com.br/image/upload/f_auto/webapp/landingV2/restaurant.png"
-            title="Restaurante"
-            textButton="Ver opções" />)
+        const { container } = render(<Card {...props} />)
 
-        // expect(screen.getByRole('ancor'))).toHaveStyle({
-        //     "background-color": theme.colors.primary
-        //     // 'font-size': '3.6rem'
-        // })
+        expect(container.firstChild).toHaveStyle({
+            backgroundColor: theme.colors.primary
+        })
+        expect(screen.getByRole("button", { name: /Ver opções/i })).toHaveStyle({
+            backgroundColor: "#cc1825"
+        })
+    })
+    it('should be render a card with variant secundary', () => {
+        const { container } = render(<Card {...props} variant="secondary" />)
+
+        expect(container.firstChild).toHaveStyle({
+            backgroundColor: theme.colors.secondary
+        })
+        expect(screen.getByRole("button", { name: /Ver opções/i })).toHaveStyle({
+            backgroundColor: "#9eb53e"
+        })
+    })
+
+    it('testing property if href is rendered', () => {
+        const { container } = render(<Card {...props} href="/page-test" />
+        )
+        expect(container.firstChild).toHaveAttribute('href', '/page-test')
     })
 })
