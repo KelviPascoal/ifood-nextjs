1dfe185ef51ec493441723fc75a0b577640842ba
kelvi <kelvi_ugo@hotmail.com>
2022-04-02 23:31:17 -0300

    feat: configura next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["static-images.ifood.com.br"]
+  }
 }
 
 module.exports = nextConfig
