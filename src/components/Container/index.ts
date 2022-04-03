f76d28af144c25d1718de51735549fbdcad03b25
kelvi <kelvi_ugo@hotmail.com>
2022-04-02 23:33:07 -0300

    refactor: altera estilo do componente Container

--- src/components/Container/index.ts
+++ src/components/Container/index.ts
@@ -3,6 +3,5 @@ import styled from "styled-components";
 export const Container = styled.div`
     max-width: 128rem;
     margin: 0 auto;
-    height: 100vh;
-    padding: 0 3rem;
+    min-width: 58.7rem;
 `;
