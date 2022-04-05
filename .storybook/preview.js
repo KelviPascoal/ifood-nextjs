744ffcad0d1f1f6fa7a976b06b437eeaceab78fd
kelvi <kelvi_ugo@hotmail.com>
2022-04-05 20:59:36 -0300

    fix: corrige a ultilizaçao de imagens no storybook que estao presentes no diretorio

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,19 @@
 import { GlobalStyles } from '../src/styles/GlobalStyles'
+
+import * as NextImage from "next/image";
+
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, "default", {
+  configurable: true,
+  value: (props) => (
+    <OriginalNextImage
+      {...props}
+      unoptimized
+    />
+  ),
+});
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
