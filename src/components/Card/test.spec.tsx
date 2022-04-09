76b0359d0d3693d603bcc7d1e0c50ff66f9e29a9
kelvi <kelvi_ugo@hotmail.com>
2022-04-09 09:33:11 -0300

    test: corrige testes de Card

--- src/components/Card/test.spec.tsx
+++ src/components/Card/test.spec.tsx
@@ -1,40 +1,47 @@
-import { screen } from "@testing-library/react"
-import { theme } from "styles/theme"
-import { render } from "utils/testUtils"
-import { Card } from "."
+import { screen } from "@testing-library/react";
+import { theme } from "styles/theme";
+import { render } from "utils/testUtils";
+import { Card } from ".";
+import userEvent from "@testing-library/user-event";
 
 const props = {
-    image: "https://static-images.ifood.com.br/image/upload/f_auto/webapp/landingV2/restaurant.png",
-    title: "Restaurante",
-    textButton: "Ver opções",
-    altImg: "ir para opções de restalrante"
-}
+  image: {
+    src: "https://static-images.ifood.com.br/image/upload/f_auto/webapp/landingV2/market.png",
+    width: 225,
+    height: 157,
+  },
+  title: "Restaurante",
+  textButton: "Ver opções",
+};
 
-describe('<Card />', () => {
-    it('should be render a card default', () => {
-        const { container } = render(<Card {...props} />)
+describe("<Card />", () => {
+  it("should be render a card default", () => {
+    const { container } = render(<Card {...props} />);
 
-        expect(container.firstChild).toHaveStyle({
-            backgroundColor: theme.colors.primary
-        })
-        expect(screen.getByRole("button", { name: /Ver opções/i })).toHaveStyle({
-            backgroundColor: "#cc1825"
-        })
-    })
-    it('should be render a card with variant secundary', () => {
-        const { container } = render(<Card {...props} variant="secondary" />)
+    expect(container.firstChild).toHaveStyle({
+      backgroundColor: theme.colors.primary,
+    });
+    expect(screen.getByText(/Ver opções/i)).toHaveStyle({
+      backgroundColor: "#cc1825",
+    });
+  });
+  it("should be render a card with secundary variant", () => {
+    const { container } = render(<Card {...props} variant="secondary" />);
 
-        expect(container.firstChild).toHaveStyle({
-            backgroundColor: theme.colors.secondary
-        })
-        expect(screen.getByRole("button", { name: /Ver opções/i })).toHaveStyle({
-            backgroundColor: "#9eb53e"
-        })
-    })
+    expect(container.firstChild).toHaveStyle({
+      backgroundColor: theme.colors.secondary,
+    });
+    expect(screen.getByText(/Ver opções/i)).toHaveStyle({
+      backgroundColor: "#9eb53e",
+    });
+  });
 
-    it('testing property if href is rendered', () => {
-        const { container } = render(<Card {...props} href="/page-test" />
-        )
-        expect(container.firstChild).toHaveAttribute('href', '/page-test')
-    })
-})
+  it("testing onClick", () => {
+    const functionTest = jest.fn();
+    render(<Card {...props} onClick={functionTest} />);
+    const card = screen.getByRole("button");
+    userEvent.click(card);
+
+    expect(functionTest).toBeCalled();
+  });
+});
