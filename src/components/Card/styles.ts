93f1c0234917fed870dd0b959590e66b818ee558
kelvi <kelvi_ugo@hotmail.com>
2022-04-09 09:26:38 -0300

    refactor: corrrige styles de Card

--- src/components/Card/styles.ts
+++ src/components/Card/styles.ts
@@ -3,7 +3,7 @@ import { CardProps } from ".";
 
 type CardStyleProps = Pick<CardProps, "variant">;
 
-const ButtonColor = {
+const ButtonColors = {
   primary: () => css`
     background-color: #cc1825;
   `,
@@ -17,36 +17,41 @@ export const Card = styled.button<CardStyleProps>`
     background-color: ${theme.colors[variant!]};
     border: 0;
     border-radius: calc(${theme.spacings.large} / 2);
-    max-width: calc(${theme.spacings.large} * 10 + 4rem);
+    max-width: calc(${theme.spacings.large} * 10 + 0.4rem);
     width: 100%;
-    height: calc(${theme.spacings.xxlarge} * 4);
+    min-height: calc(${theme.spacings.large} * 5 + 1.4rem);
     position: relative;
     text-decoration: none;
     transition: background-color ${theme.transition.fast};
     cursor: pointer;
+    padding-top: calc(${theme.spacings.xsmall} + 0.2rem);
+    padding-right: 0;
+    padding-bottom: calc(${theme.spacings.medium} + 0.4rem);
+    padding-left:  calc(${theme.spacings.large} / 2);
 
     display: flex;
     justify-content: space-between;
     flex-direction: column;
     align-items: flex-start;
-    padding-top: calc(${theme.spacings.xsmall} + 0.2rem);
-    padding-right: 0;
-    padding-bottom: calc(${theme.spacings.medium} + 0.4rem);
-    padding-left:  calc(${theme.spacings.large} / 2);
     
+    
+    @media(max-width: ${theme.breakPoints.tablet}) {
+      max-width: calc(${theme.spacings.medium} * 10 + 1rem);
+    }
 
     @media(max-width: ${theme.breakPoints.moblideLarge}) {
       align-items: center;
-      padding: 2.4rem 0;
-      max-width: 18.3rem;
+      padding: ${theme.spacings.small} 0;
+      max-width: calc(${theme.spacings.large} * 5 - 1.7rem);
+      min-height: calc(${theme.spacings.large} * 5 - 2rem);
     }
 
     @media(max-width: ${theme.breakPoints.mobileMedium}) {
-      max-width: 16.3rem;
+      max-width: calc(${theme.spacings.xsmall} * 10 + 0.3rem);
     }
 
     @media(max-width: ${theme.breakPoints.moblideSmall}) {
-      max-width: 13.8rem;
+      max-width: calc(${theme.spacings.xsmall} * 10 - 2.2rem);
     }
     }
   `}
@@ -58,18 +63,22 @@ export const CardImage = styled.div`
     right: 0;
     bottom: 0;
 
+    @media (max-width: ${theme.breakPoints.tablet}) {
+      max-width: calc(${theme.spacings.small} * 10 - 3rem);
+    }
+
     @media (max-width: ${theme.breakPoints.moblideLarge}) {
       position: static;
-      max-width: 18.3rem;
+      max-width: calc(${theme.spacings.xsmall} * 10 + 2.3rem);
       margin-top: auto;
     }
 
     @media (max-width: ${theme.breakPoints.mobileMedium}) {
-      max-width: 12.3rem;
+      max-width: calc(${theme.spacings.large} * 3) + 0.3rem;
     }
 
     @media (max-width: ${theme.breakPoints.moblideSmall}) {
-      max-width: 9.8rem;
+      max-width: calc(${theme.spacings.large} * 2 + 1.8rem);
     }
   `}
 `;
@@ -80,27 +89,30 @@ export const CardButton = styled.span<CardStyleProps>`
     font-weight: ${theme.font.bold};
     font-size: ${theme.font.sizes.small};
     height: calc(${theme.spacings.medium} + 0.3rem);
+    text-align: center;
     border: 0;
     border-radius: calc(${theme.border.radius} * 2);
-    ${ButtonColor[variant!]};
+    padding: 0 calc(${theme.spacings.xsmall} - 0.4rem);
+    ${ButtonColors[variant!]};
     cursor: pointer;
 
     display: flex;
     align-items: center;
     justify-content: center;
 
-    @media (max-width: ${theme.breakPoints.tablet}) {
+    @media (max-width: ${theme.breakPoints.moblideLarge}) {
       font-size: ${theme.font.sizes.small};
-      width: 80%;
+      width: 90%;
     }
 
     svg {
       font-size: ${theme.font.sizes.medium};
-      margin-left: 1rem;
-      margin-top: 0.3rem;
+      margin-left: calc(${theme.spacings.xxsmall} + 0.2rem);
+      margin-top: calc(${theme.spacings.xxsmall} - 0.5rem);
 
       @media (max-width: ${theme.breakPoints.tablet}) {
         font-size: ${theme.font.sizes.small};
+        margin-left: ${theme.spacings.xxsmall};
       }
     }
   `}
