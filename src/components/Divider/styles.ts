deb08758298ac4e331d6e62ca150941a88bb9fdc
kelvi <kelvi_ugo@hotmail.com>
2022-04-09 09:45:04 -0300

    refactor: atualiza styles para o componente Divider

--- src/components/Divider/styles.ts
+++ src/components/Divider/styles.ts
@@ -1,14 +1,14 @@
-import { DividerProps } from './index';
-import { lighten } from 'polished';
+import { DividerProps } from "./index";
+import { lighten } from "polished";
 import styled, { css } from "styled-components";
 
 export const DividerStyle = styled.div<DividerProps>`
-    ${({theme, marginBotton, marginTop}) => css`
-    background-color: ${ lighten(0.2,theme.font.colors.gray)};
+  ${({ theme, marginBottom, marginTop }) => css`
+    background-color: ${lighten(0.2, theme.font.colors.gray)};
     width: 100%;
     height: 0.1rem;
 
-    margin-top: ${marginTop ? marginTop : 0};
-    margin-bottom: ${marginBotton ? marginBotton : 0};
-    `}
-`
+    margin-top: ${!!marginTop ? theme.spacings[marginTop] : 0};
+    margin-bottom: ${!!marginBottom ? theme.spacings[marginBottom] : 0};
+  `}
+`;
