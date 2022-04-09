f6a9f4e2aec58e7dfb48981767899e9903b55f99
kelvi <kelvi_ugo@hotmail.com>
2022-04-09 14:57:32 -0300

    test: finaliza testes de MiniCard

--- src/components/MiniCard/test.spec.tsx
+++ src/components/MiniCard/test.spec.tsx
@@ -1,57 +1,71 @@
 import { theme } from "styles/theme";
 import { render } from "utils/testUtils";
-import { screen } from "@testing-library/react"
+import { screen } from "@testing-library/react";
 import { MiniCard } from ".";
 import userEvent from "@testing-library/user-event";
 
+const imageMock = {
+  src: "/img/drinks.webp",
+  height: 95,
+  width: 140,
+  alt: "imagem de duas bebidas, refrigerante e cerveja",
+};
 
 describe("<MiniCard />", () => {
-    it('should be render a miniCard yellow', () => {
-        const { container } = render(<MiniCard variant="drinks" />)
-        
-        expect(container.firstChild?.firstChild).toHaveStyle({
-            backgroundColor: theme.colors.yellow
-        })
-    })
+  it("should be render a miniCard yellow", () => {
+    const { container } = render(
+      <MiniCard variant="yellow" image={imageMock} text="test" />
+    );
 
-    it('should be render a miniCard pink', () => {
-        const { container } = render(<MiniCard variant="pharmacy" />)
-        
-        expect(container.firstChild?.firstChild).toHaveStyle({
-            backgroundColor: theme.colors.pink
-        })
-    })
+    expect(container.firstChild?.firstChild).toHaveStyle({
+      backgroundColor: theme.colors.yellow,
+    });
+  });
 
-    it('should be render a miniCard red', () => {
-        const { container } = render(<MiniCard variant="express" />)
-        
-        expect(container.firstChild?.firstChild).toHaveStyle({
-            backgroundColor: theme.colors.red
-        })
-    })
+  it("should be render a miniCard pink", () => {
+    const { container } = render(
+      <MiniCard variant="pink" image={imageMock} text="test" />
+    );
 
-    it('should be render a miniCard purpure', () => {
-        const { container } = render(<MiniCard variant="petshop" />)
-        
-        expect(container.firstChild?.firstChild).toHaveStyle({
-            backgroundColor: theme.colors.purpure
-        })
-    })
+    expect(container.firstChild?.firstChild).toHaveStyle({
+      backgroundColor: theme.colors.pink,
+    });
+  });
 
-    it('testing onClick', () => {
-        const functionTest = jest.fn();
-        render(<MiniCard data-testid="mini-card" onClick={functionTest} variant="drinks" />)
-        
-        userEvent.click(screen.getByTestId('mini-card'))
+  it("should be render a miniCard red", () => {
+    const { container } = render(
+      <MiniCard variant="red" image={imageMock} text="test" />
+    );
 
-        expect(functionTest).toBeCalled()
-    })
+    expect(container.firstChild?.firstChild).toHaveStyle({
+      backgroundColor: theme.colors.red,
+    });
+  });
 
-    it('testing if hover change component size', () => {
-        const functionTest = jest.fn();
-        render(<MiniCard data-testid="mini-card" onClick={functionTest} variant="drinks" />)
-        
-        userEvent.click(screen.getByTestId('mini-card'))
-        expect(functionTest).toBeCalled()
-    })
-})
+  it("should be render a miniCard purpure", () => {
+    const { container } = render(
+      <MiniCard variant="purpure" image={imageMock} text="test" />
+    );
+
+    expect(container.firstChild?.firstChild).toHaveStyle({
+      backgroundColor: theme.colors.purpure,
+    });
+  });
+
+  it("testing onClick", () => {
+    const functionTest = jest.fn();
+    render(
+      <MiniCard
+        data-testid="mini-card"
+        onClick={functionTest}
+        variant="yellow"
+        image={imageMock}
+        text="test"
+      />
+    );
+
+    userEvent.click(screen.getByTestId("mini-card"));
+
+    expect(functionTest).toBeCalled();
+  });
+});
