9deaa1ed1d53e6138d2a1b66a52aece1c155a86d
kelvi <kelvi_ugo@hotmail.com>
2022-04-11 10:36:12 -0300

    feat: altera title da aplicaçao

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,22 @@
-import type { AppProps } from 'next/app';
-import { ThemeProvider } from 'styled-components';
-import { GlobalStyles } from '../styles/GlobalStyles';
-import { theme } from '../styles/theme';
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "../styles/GlobalStyles";
+import { theme } from "../styles/theme";
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <Component {...pageProps} />
-    </ThemeProvider>)
+    <>
+      <Head>
+        <title>Delivery de Comida e Mercado - iFood</title>
+      </Head>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </ThemeProvider>
+      )
+    </>
+  );
 }
 
-export default App
+export default App;
